c4359a9d4458667120e69dc3d3032a27c9f1b3d1
Chathuranga Dassanayake <chathura2ihl@gmail.com>
2019-11-05 21:36:22 +0530

    add arrow heads, prevent collisions

--- src/common/force_directed_graph/simulation.tsx
+++ src/common/force_directed_graph/simulation.tsx
@@ -2,8 +2,10 @@ import {
   forceCenter,
   forceLink,
   forceManyBody,
-  forceSimulation
+  forceSimulation,
+  forceCollide
 } from "d3-force";
+import { interpolateObject } from "d3-interpolate";
 import { SimulationProps } from "./types";
 
 /**
@@ -24,6 +26,7 @@ export function generateSimulation(props: SimulationProps) {
     .force("link", forceLink().id((d: any) => d.id))
     .force("charge", forceManyBody().strength(strength))
     .force("center", forceCenter(width / 2, height / 2))
+    .force("collision", forceCollide().radius(() => 50))
     .stop();
 
   (simulation.force("link") as any).links(links);
@@ -34,6 +37,27 @@ export function generateSimulation(props: SimulationProps) {
   for (let i = 0; i < Math.min(maxSteps, upperBound); ++i) {
     simulation.tick();
   }
-
   return { nodes, links };
+}
+
+export function mapLinkToArrowHead(distanceFromNode: number) {
+  return ({ source, target }: any) => {
+    const { x: x1, y: y1, id: sourceId } = source;
+    const { x: x2, y: y2, id: targetId } = target;
+    const distance = getDistance(source, target);
+    const step = distanceFromNode / distance;
+    const i = interpolateObject({ x: x1, y: y1 }, { x: x2, y: y2 });
+    const pos = i(step);
+    const dirCorrection =
+      (x1 > x2 && y1 > y2) || (x1 > x2 && y1 < y2) ? 180 : 0;
+    const angle = (Math.atan(-(y2 - y1) / (x2 - x1)) * 180) / Math.PI;
+    const key = `${sourceId}_${targetId}`;
+    return { ...pos, angle: angle + dirCorrection };
+  };
+}
+
+function getDistance(source: any, target: any) {
+  const { x: x1, y: y1 } = source;
+  const { x: x2, y: y2 } = target;
+  return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
 }
