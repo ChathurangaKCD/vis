5a1592f266d6cd0d7c3e4fbf5783383dc1f7192f
Chathuranga Dassanayake <chathura2ihl@gmail.com>
2019-11-06 21:29:57 +0530

    wrap inputs in formik Field

---
+++ src/common/formik/index.tsx
@@ -0,0 +1,3 @@
+export * from "./checkbox_input";
+export * from "./radio_input";
+export * from "./text_input";
---
+++ src/common/formik/radio_input.tsx
@@ -0,0 +1,32 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Radio,
+  RadioGroup
+} from "@chakra-ui/core";
+import { useField } from "formik";
+import React from "react";
+
+export function RadioInput(props: RadioInputProps) {
+  const { name, label, items, validate, isRequired = false } = props;
+  const [field, meta] = useField({ name, validate });
+  const { error, touched } = meta;
+  const isInvalid = error ? touched : false;
+  const { value: fieldValue, onChange } = field;
+  const radioOnChange = ({ target: { value } }: any) =>
+    onChange({ target: { name, value } });
+  return (
+    <FormControl as="fieldset" isInvalid={isInvalid} isRequired>
+      <FormLabel as="legend">{label}</FormLabel>
+      <RadioGroup value={fieldValue as any} onChange={radioOnChange}>
+        {items.map(({ label, value }) => (
+          <Radio value={value} key={value}>
+            {label}
+          </Radio>
+        ))}
+      </RadioGroup>
+      <FormErrorMessage>{error}</FormErrorMessage>
+    </FormControl>
+  );
+}
