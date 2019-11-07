9aa5844b22a0c256b97c65c26b3b0d4f37eae845
Chathuranga Dassanayake <chathura2ihl@gmail.com>
2019-11-07 12:15:15 +0530

    service reload, list ui, refactor service editor

--- src/views/services/index.tsx
+++ src/views/services/index.tsx
@@ -1,12 +1,11 @@
-import { Flex, RadioButtonGroup, Stack, useDisclosure } from "@chakra-ui/core";
+import { Button, Flex, RadioButtonGroup, Stack, Box } from "@chakra-ui/core";
 import React, { useEffect, useState } from "react";
 import { DrawerWrapper } from "../../common/drawer";
 import { RadioButton } from "../../common/radio_button";
+import { useStoreActions } from "../../store/hooks";
 import { ServiceEditor } from "./editor/service_editor";
 import { ServiceGraph } from "./graph/service_graph";
 import { ServiceList } from "./list/service_list";
-import { useStoreActions } from "../../store/hooks";
-import { ServiceID } from "../../types/service";
 import { FormUIStateProvider, useFormUiContext } from "./state_provider";
 
 type ViewType = "list" | "graph";
@@ -32,9 +31,16 @@ function ServicesViewC() {
   return (
     <>
       <Stack h="100%" w="100%">
-        <Flex flexBasis="auto" justify="center">
-          <ViewTypeSelect onTypeSelect={setViewType}></ViewTypeSelect>
-        </Flex>
+        <Stack isInline w="50%" justifyContent="center" spacing={10}>
+          <Box>
+            <ViewTypeSelect onTypeSelect={setViewType}></ViewTypeSelect>
+          </Box>
+          <Box>
+            <Button onClick={() => reloadServices()} size="sm">
+              Refresh All
+            </Button>
+          </Box>
+        </Stack>
         <ViewContent />
       </Stack>
       <DrawerWrapper title="Update" isOpen={isOpen} onClose={onClickDiscard}>
